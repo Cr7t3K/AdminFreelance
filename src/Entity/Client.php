686ff39d50c67fcd7a531a77972880dd9e740faa
Guillaume <cguillaumedu85@orange.fr>
2021-06-17 15:02:09 +0200

    Create Client entity

---
+++ src/Entity/Client.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ClientRepository::class)
+ */
+class Client
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     * @Assert\Length(max=100)
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     * @Assert\Length(max=100)
+     */
+    private string $address;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     * @Assert\Length(max=100)
+     */
+    private ?string $contact;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     * @Assert\Length(max=50)
+     */
+    private ?string $tva;
+
+    /**
+     * @ORM\Column(type="string", length=5, nullable=true)
+     * @Assert\Regex("/^[\d]{4}+[a-z]$/")
+     */
+    private ?string $ape;
+
+    /**
+     * @ORM\Column(type="string", length=9)
+     * @Assert\Regex("/^[\d]{14}$/")
+     */
+    private string $siret;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\Email()
+     */
+    private string $email;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     * @Assert\Regex("/^[\d]*$/")
+     */
+    private ?string $phone;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getContact(): ?string
+    {
+        return $this->contact;
+    }
+
+    public function setContact(?string $contact): self
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    public function getTva(): ?string
+    {
+        return $this->tva;
+    }
+
+    public function setTva(?string $tva): self
+    {
+        $this->tva = $tva;
+
+        return $this;
+    }
+
+    public function getApe(): ?string
+    {
+        return $this->ape;
+    }
+
+    public function setApe(?string $ape): self
+    {
+        $this->ape = $ape;
+
+        return $this;
+    }
+
+    public function getSiret(): ?string
+    {
+        return $this->siret;
+    }
+
+    public function setSiret(string $siret): self
+    {
+        $this->siret = $siret;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+}
